4932d72ea400a3ad5834ec9154400d900b6e3a77
Aditya Chauhan <chauhanaditya0107@gmail.com>
2026-03-23 23:06:53 +0530

    updated news fetcher file from live news

---
+++ src/news_fetcher.py
@@ -0,0 +1,47 @@
+from newsapi import NewsApiClient
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("NEWS_API_KEY")
+
+if not API_KEY:
+    raise ValueError("API key not found")
+
+newsapi = NewsApiClient(api_key=API_KEY)
+
+def fetch_news(query="AAPL"):
+    articles = newsapi.get_everything(
+        q = query,
+        language='en',
+        sort_by='publishedAt',
+        page_size=20
+    )
+    
+    data = []
+
+    for article in articles['articles']:
+        data.append({
+            "Date": article['publishedAt'][:10],
+            "Title" : article['title']
+        })
+
+    return pd.DataFrame(data)
+
+print("Fetching news")
+
+df = fetch_news("AAPL")
+
+print(df.head())
+
+os.makedirs("data", exist_ok=True)
+
+file_path = "data/news_data.csv"
+df.to_csv(file_path, index=False)
+
+print("News data saved at:", file_path)
+
+
+
